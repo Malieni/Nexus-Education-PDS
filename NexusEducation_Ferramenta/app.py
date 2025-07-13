2a195dcd6b70343bf479e950a6d56a2d6dc7adf6
Malieni <gui.souza.malieni@gmail.com>
2025-07-13 13:14:41 -0300

    Definindo a porta para o render

--- NexusEducation_Ferramenta/app.py
+++ NexusEducation_Ferramenta/app.py
@@ -285,4 +285,5 @@ with gr.Blocks(theme='shivi/calm_seafoam') as app:
 #    app.launch(share=True)
 
 if __name__=="__main__":
-    app.launch(server_name="0.0.0.0", server_port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.launch(server_name="0.0.0.0", server_port=port)
